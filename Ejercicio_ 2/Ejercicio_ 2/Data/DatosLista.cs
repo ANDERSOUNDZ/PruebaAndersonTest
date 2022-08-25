13b30429189a8f1c3521ee1e68678dde87494b4a
ANDERSOUNDZ <andersonmikol@live.com>
2022-08-24 21:00:16 -0500

    Ejercicio 2

---
+++ Ejercicio_ 2/Ejercicio_ 2/Data/DatosLista.cs
@@ -0,0 +1,22 @@
+﻿using Ejercicio__2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio__2.Data
+{
+    internal class DatosLista
+    {
+        public List<Persona> ListaPersona()
+        {
+            return new List<Persona> 
+            {
+                new Persona {Nombre="Patricia",Apellido="Fernandez",Edad=32},
+                new Persona {Nombre="Beatriz",Apellido="Pinzon",Edad=16},
+                new Persona {Nombre="Berta",Apellido="Contreras",Edad=25}
+            };
+        }
+    }
+}
---
+++ Ejercicio_ 2/Ejercicio_ 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using Ejercicio__2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio__2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonasLista();
+        }
+
+        static double PersonasLista()
+        {
+            
+            Persona persona = new Persona();
+            if (persona.Edad >= 18)
+            {
+                 return Console.WriteLine( persona.Apellido + persona.Nombre + persona.Edad); 
+            }
+            else {
+                return Console.WriteLine("Mujeres menores a -18");
+            }
+                        
+        }
+    }
+}
